a2a8dfec273a5d1178b8839760ab1b9e5d2db9a1
Artem Erusov <erusovav@gmail.com>
2024-05-23 20:34:04 +0300

    styled the code

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,13 @@ import UltimateReact from './pages/ultimateReact/ultimateReact';
 // import Course from "./pages/course/course"
 
 function App() {
-   return (
-      <Fragment>
-         <Header />
-         <UltimateReact />
-         {/* <Course/> */}
-      </Fragment>
-   );
+  return (
+    <Fragment>
+      <Header />
+      <UltimateReact />
+      {/* <Course/> */}
+    </Fragment>
+  );
 }
 
 export default App;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,21 +2,21 @@ import styles from './styles.module.scss';
 import { Fragment } from 'react';
 
 const Header = () => {
-   return (
-      <Fragment>
-         <div className={styles.header}>
-            <ul className={styles.list}>
-               <li className={styles.list__item}>Мини задачи</li>
-               <li className={styles.list__item}>Toolkit</li>
-               <li className={styles.list__item}>Yandex</li>
-               <li className={styles.list__item}>Переделки с JS</li>
-               <li className={styles.list__item}>Ui элементы</li>
-               <li className={styles.list__item + ' ' + styles.list__item_active}>Ultimate React</li>
-               <li className={styles.list__item}>Курсы</li>
-            </ul>
-         </div>
-      </Fragment>
-   );
+  return (
+    <Fragment>
+      <div className={styles.header}>
+        <ul className={styles.list}>
+          <li className={styles.list__item}>Мини задачи</li>
+          <li className={styles.list__item}>Toolkit</li>
+          <li className={styles.list__item}>Yandex</li>
+          <li className={styles.list__item}>Переделки с JS</li>
+          <li className={styles.list__item}>Ui элементы</li>
+          <li className={styles.list__item + ' ' + styles.list__item_active}>Ultimate React</li>
+          <li className={styles.list__item}>Курсы</li>
+        </ul>
+      </div>
+    </Fragment>
+  );
 };
 
 export default Header;
