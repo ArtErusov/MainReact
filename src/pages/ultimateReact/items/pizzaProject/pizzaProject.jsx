0c3908ea11a45a05bf79b4c704ed2834245514e5
Artem Erusov <erusovav@gmail.com>
2024-05-24 22:24:44 +0300

    made a block for working with react

--- src/pages/ultimateReact/items/pizzaProject/pizzaProject.jsx
+++ src/pages/ultimateReact/items/pizzaProject/pizzaProject.jsx
@@ -1,58 +1,62 @@
 import styles from './styles.module.scss';
 import { Fragment } from 'react';
-import focacciaIMG from '../../../../assets/img/pizzas/focaccia.jpg';
+// import focacciaIMG from '../../../../assets/img/pizzas/focaccia.jpg';
 
 const PizzaProject = () => {
-  const pizzaData = [
-    {
-      name: 'Focaccia',
-      ingredients: 'Bread with italian olive oil and rosemary',
-      price: 6,
-      photoName: 'pizzas/focaccia.jpg',
-      soldOut: false,
-    },
-    {
-      name: 'Pizza Margherita',
-      ingredients: 'Tomato and mozarella',
-      price: 10,
-      photoName: 'pizzas/margherita.jpg',
-      soldOut: false,
-    },
-    {
-      name: 'Pizza Spinaci',
-      ingredients: 'Tomato, mozarella, spinach, and ricotta cheese',
-      price: 12,
-      photoName: 'pizzas/spinaci.jpg',
-      soldOut: false,
-    },
-    {
-      name: 'Pizza Funghi',
-      ingredients: 'Tomato, mozarella, mushrooms, and onion',
-      price: 12,
-      photoName: 'pizzas/funghi.jpg',
-      soldOut: false,
-    },
-    {
-      name: 'Pizza Salamino',
-      ingredients: 'Tomato, mozarella, and pepperoni',
-      price: 15,
-      photoName: 'pizzas/salamino.jpg',
-      soldOut: true,
-    },
-    {
-      name: 'Pizza Prosciutto',
-      ingredients: 'Tomato, mozarella, ham, aragula, and burrata cheese',
-      price: 18,
-      photoName: 'pizzas/prosciutto.jpg',
-      soldOut: false,
-    },
-  ];
+  // const pizzaData = [
+  //   {
+  //     name: 'Focaccia',
+  //     ingredients: 'Bread with italian olive oil and rosemary',
+  //     price: 6,
+  //     photoName: 'pizzas/focaccia.jpg',
+  //     soldOut: false,
+  //   },
+  //   {
+  //     name: 'Pizza Margherita',
+  //     ingredients: 'Tomato and mozarella',
+  //     price: 10,
+  //     photoName: 'pizzas/margherita.jpg',
+  //     soldOut: false,
+  //   },
+  //   {
+  //     name: 'Pizza Spinaci',
+  //     ingredients: 'Tomato, mozarella, spinach, and ricotta cheese',
+  //     price: 12,
+  //     photoName: 'pizzas/spinaci.jpg',
+  //     soldOut: false,
+  //   },
+  //   {
+  //     name: 'Pizza Funghi',
+  //     ingredients: 'Tomato, mozarella, mushrooms, and onion',
+  //     price: 12,
+  //     photoName: 'pizzas/funghi.jpg',
+  //     soldOut: false,
+  //   },
+  //   {
+  //     name: 'Pizza Salamino',
+  //     ingredients: 'Tomato, mozarella, and pepperoni',
+  //     price: 15,
+  //     photoName: 'pizzas/salamino.jpg',
+  //     soldOut: true,
+  //   },
+  //   {
+  //     name: 'Pizza Prosciutto',
+  //     ingredients: 'Tomato, mozarella, ham, aragula, and burrata cheese',
+  //     price: 18,
+  //     photoName: 'pizzas/prosciutto.jpg',
+  //     soldOut: false,
+  //   },
+  // ];
 
   return (
     <Fragment>
       <div className={styles.mainContainer}>
-        <p>{pizzaData[0].ingredients}</p>
-        <img src={focacciaIMG} alt="" />
+        <h2>fast react pizza co.</h2>
+        {/* <p>{pizzaData[0].ingredients}</p> */}
+        {/* <img src={focacciaIMG} alt="" /> */}
+        <p>Aauthentic Italian cuisine. 6 creative dishes to choose from. All from our stone oven, all organic, all delicious.</p>
+        <p>We`re open from 12:00 to 22:00. come visit us or order online.</p>
+        <button>Order</button>
       </div>
       <div className={styles.extraContainer}>
         <p>Pizza Project</p>
