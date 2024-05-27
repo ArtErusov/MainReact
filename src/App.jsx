79a0717e1b37f57bd5ad01616ce229cdd0e8f12a
Artem Erusov <erusovav@gmail.com>
2024-05-27 22:17:56 +0300

    Added a router

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { Fragment } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './assets/css/main.css';
 import Header from './components/header/Header';
 import UltimateReact from './pages/ultimateReact/ultimateReact';
 import Yandex from './pages/yandex/miniApp';
+
 // import MiniApp from "./pages/miniApp/miniApp"
 // import Course from "./pages/course/course"
 
@@ -10,9 +12,11 @@ function App() {
   return (
     <Fragment>
       <Header />
-      <UltimateReact />
-      {/* <Course/> */}
-      <Yandex />
+      <Routes>
+        <Route path="/ultimatereact" element={<UltimateReact />} />
+        <Route path="/yandex" element={<Yandex />} />
+        {/* <Course/> */}
+      </Routes>
     </Fragment>
   );
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,17 +1,23 @@
-import styles from './styles.module.scss';
+import { Link } from 'react-router-dom';
 import { Fragment } from 'react';
+import styles from './styles.module.scss';
+
 
 const Header = () => {
   return (
     <Fragment>
       <div className={styles.header}>
         <ul className={styles.list}>
-          {/* <li className={styles.list__item}>Мини задачи</li> */}
-          {/* <li className={styles.list__item}>Toolkit</li> */}
-          <li className={styles.list__item}>Yandex</li>
+          <li className={styles.list__item}>
+            <Link to="http://localhost:5173/yandex">Yandex</Link>
+          </li>
+          <li className={styles.list__item + ' ' + styles.list__item_active}>
+            <Link to="http://localhost:5173/ultimatereact">Ultimate React</Link>
+          </li>
           {/* <li className={styles.list__item}>Переделки с JS</li> */}
           {/* <li className={styles.list__item}>Ui элементы</li> */}
-          <li className={styles.list__item + ' ' + styles.list__item_active}>Ultimate React</li>
+          {/* <li className={styles.list__item}>Мини задачи</li> */}
+          {/* <li className={styles.list__item}>Toolkit</li> */}
           {/* <li className={styles.list__item}>Курсы</li> */}
         </ul>
       </div>
