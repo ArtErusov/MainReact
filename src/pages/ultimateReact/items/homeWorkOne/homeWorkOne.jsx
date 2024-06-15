36a0f3443a0bba48d181e0adc04e8adc3e19dae8
Artem Erusov <erusovav@gmail.com>
2024-06-15 17:35:27 +0300

    made 1 part of the homeWorkOne component as part of homework

--- src/pages/ultimateReact/items/homeWorkOne/homeWorkOne.jsx
+++ src/pages/ultimateReact/items/homeWorkOne/homeWorkOne.jsx
@@ -1,21 +1,44 @@
 import styles from './styles.module.scss';
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 import CardBottom from '../../../../components/UI/cardBottom/cardBottom';
 
 const HomeWorkOne = () => {
   const сomponentState = {
-    title: 'Заготовка для домашних работ',
+    title: 'Work home 1',
     revision: 0,
     ready: 0,
-    column: 4,
-    height: '472px', 
-    width: '587px'
+    column: 2,
+    height: '472px',
+    width: '274px',
   };
+
+  const dayOfTheWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  const [step, setStep] = useState(0);
+  const [counter, setCounter] = useState(1);
+  const date = new Date();
+  date.setDate(date.getDate() + step);
 
   return (
     <Fragment>
-      <div style={{height:сomponentState.height, width:сomponentState.width}} className={styles.mainContainer}>
-    {/* контент */}
+      <div style={{ height: сomponentState.height, width: сomponentState.width }} className={styles.mainContainer}>
+        {/* Задание 1 */}
+        <div className={styles.counter}>
+          <button onClick={() => setStep(step + counter)}>+</button>
+          <div>step: {step}</div>
+          <button onClick={() => setStep(step - counter)}>--</button>
+        </div>
+        <div className={styles.counter}>
+          <button onClick={() => setCounter(counter + 1)}>+</button>
+          <div>counter: {counter}</div>
+          <button onClick={() => setCounter(counter - 1)}>--</button>
+        </div>
+        <div onClick={() => setStep(0)} className={styles.counter_date}>
+          <p>Today is {dayOfTheWeek[date.getDay() - 1]}</p>
+          <p>
+            {date.getDate()} {months[date.getMonth()]} {date.getFullYear()}
+          </p>
+        </div>
       </div>
       <CardBottom title={сomponentState.title} revision={сomponentState.revision} ready={сomponentState.ready} column={сomponentState.column} />
     </Fragment>
