ae3acea467029a3739ab465d9fb5b3e6f2e9699a
Artem Erusov <erusovav@gmail.com>
2024-06-15 14:47:43 +0300

    Optimized the reactionToButtons component

--- src/pages/ultimateReact/items/reactionToButtons/reactionToButtons.jsx
+++ src/pages/ultimateReact/items/reactionToButtons/reactionToButtons.jsx
@@ -3,45 +3,49 @@ import { Fragment, useState } from 'react';
 import CardBottom from '../../../../components/UI/cardBottom/cardBottom';
 
 const ReactionToButtons = () => {
-  const [state, setState] = useState(1);
   const сomponentState = {
     title: 'Reaction to buttons',
     revision: 0,
-    ready: 0,
+    ready: 1,
     column: 2,
     height: '472px',
     width: '273px',
   };
 
-  console.log(state);
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+
+  console.log(step);
   return (
     <Fragment>
       <div style={{ height: сomponentState.height, width: сomponentState.width }} className={styles.mainContainer}>
-        <div className={styles.steps}>
-          <div className={styles.numbers}>
-            <div className={styles.numbers_btn + ' ' + styles.active}>1</div>
-            <div className={state < 2 ? styles.numbers_btn : styles.numbers_btn + ' ' + styles.active}>2</div>
-            <div className={state < 3 ? styles.numbers_btn : styles.numbers_btn + ' ' + styles.active}>3</div>
+        <button onClick={() => setIsOpen(!isOpen)} className={isOpen ? styles.open : styles.close}>
+          {isOpen ? 'close' : 'open'}
+        </button>
+        {isOpen && (
+          <div>
+            <div className={styles.numbers}>
+              <div className={styles.numbers_btn + ' ' + styles.active}>1</div>
+              <div className={step < 2 ? styles.numbers_btn : styles.numbers_btn + ' ' + styles.active}>2</div>
+              <div className={step < 3 ? styles.numbers_btn : styles.numbers_btn + ' ' + styles.active}>3</div>
+            </div>
+            <p className={styles.step}>Step {step} :</p>
+
+            <p className={styles.message}>
+              {step === 1 && 'Learn React😍'}
+              {step === 2 && 'Apply for jobs👨‍💻'}
+              {step === 3 && 'Invest your new income👩‍🚀'}
+            </p>
+            <div className={styles.buttons}>
+              <button onClick={() => (step > 1 ? setStep(step - 1) : setStep(1))} className={styles.buttons_previos}>
+                previos
+              </button>
+              <button onClick={() => (step < 3 ? setStep(step + 1) : setStep(3))} className={styles.buttons_next}>
+                next
+              </button>
+            </div>
           </div>
-          <p className={styles.step}>
-            {state === 1 && 'Step 1:'}
-            {state === 2 && 'Step 2:'}
-            {state === 3 && 'Step 3:'}
-          </p>
-          <p className={styles.message}>
-            {state === 1 && 'Learn React😍'}
-            {state === 2 && 'Apply for jobs👨‍💻'}
-            {state === 3 && 'Invest your new income👩‍🚀'}
-          </p>
-          <div className={styles.buttons}>
-            <button onClick={() => (state > 1 ? setState(state - 1) : setState(1))} className={styles.buttons_previos}>
-              previos
-            </button>
-            <button onClick={() => (state < 3 ? setState(state + 1) : setState(3))} className={styles.buttons_next}>
-              next
-            </button>
-          </div>
-        </div>
+        )}
       </div>
       <CardBottom title={сomponentState.title} revision={сomponentState.revision} ready={сomponentState.ready} column={сomponentState.column} />
     </Fragment>
