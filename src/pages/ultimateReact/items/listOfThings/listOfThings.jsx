94e62ab3c89ad9199536c02897c6158c1282a9b4
Artem Erusov <erusovav@gmail.com>
2024-06-18 19:42:14 +0300

    Worked on the listOfThings component

--- src/pages/ultimateReact/items/listOfThings/listOfThings.jsx
+++ src/pages/ultimateReact/items/listOfThings/listOfThings.jsx
@@ -9,20 +9,35 @@ const ListOfThings = () => {
     ready: 0,
     column: 6,
     height: '472px',
-    width: '900px'
+    width: '900px',
   };
 
+  const arrayItems = [];
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    
+  };
+  //Создал массив с числами от 1 до 10
+  const createArray = Array.from({ length: 10 }, (_, i) => i + 1);
   return (
     <Fragment>
-      <div style={{height:сomponentState.height, width:сomponentState.width}} className={styles.mainContainer}>
-      <div className={styles.header}>
-        <div className={styles.header__text}>What do you need for your trip?</div>
-        <input type="number" className={styles.header__number} min={0} max={10} placeholder='0'/>
-        <input type="text" className={styles.header__inpText} placeholder='item.....'/>
-        <button className={styles.header__button}>add</button>
-      </div>
-      <div className={styles.main}></div>
-      <div className={styles.bottom}></div>
+      <div style={{ height: сomponentState.height, width: сomponentState.width }} className={styles.mainContainer}>
+
+        <form className={styles.header} onSubmit={handleSubmit}>
+          <div className={styles.header__text}>What do you need for your trip?</div>
+          <select id="" className={styles.header__number}>
+            {createArray.map((num) => (
+              <option value={num} key={num}>
+                {num}
+              </option>
+            ))}
+          </select>
+          <input type="text" className={styles.header__inpText} placeholder="item....." />
+          <button className={styles.header__button}>add</button>
+        </form>
+
+        <div className={styles.main}></div>
+        <div className={styles.bottom}></div>
       </div>
       <CardBottom title={сomponentState.title} revision={сomponentState.revision} ready={сomponentState.ready} column={сomponentState.column} />
     </Fragment>
