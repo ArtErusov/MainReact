e817d52e40429532be3511b1ce0882f75875b478
Artem Erusov <erusovav@gmail.com>
2024-06-19 12:35:13 +0300

    добавил работу с input в компонент listOfThings

--- src/pages/ultimateReact/items/listOfThings/listOfThings.jsx
+++ src/pages/ultimateReact/items/listOfThings/listOfThings.jsx
@@ -1,5 +1,5 @@
 import styles from './styles.module.scss';
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 import CardBottom from '../../../../components/UI/cardBottom/cardBottom';
 
 const ListOfThings = () => {
@@ -11,28 +11,40 @@ const ListOfThings = () => {
     height: '472px',
     width: '900px',
   };
-
+  const [description, setDescription] = useState('');
+  const [quantity, setQuantitty] = useState(1);
   const arrayItems = [];
   const handleSubmit = (event) => {
     event.preventDefault();
-    
+    if (!description) return;
+    const newItem ={description, quantity, packed: false, id: Date.now()}
+    console.log(newItem);
+    setDescription('');
+    setQuantitty(1);
   };
+  
   //Создал массив с числами от 1 до 10
   const createArray = Array.from({ length: 10 }, (_, i) => i + 1);
   return (
     <Fragment>
       <div style={{ height: сomponentState.height, width: сomponentState.width }} className={styles.mainContainer}>
-
         <form className={styles.header} onSubmit={handleSubmit}>
           <div className={styles.header__text}>What do you need for your trip?</div>
-          <select id="" className={styles.header__number}>
-            {createArray.map((num) => (
-              <option value={num} key={num}>
-                {num}
-              </option>
-            ))}
+          <select 
+            value={quantity} id="" 
+            className={styles.header__number} 
+            onChange={(e) => setQuantitty(Number(e.target.value))}>
+              {createArray.map((num) => (
+                <option value={num} key={num}>
+                  {num}
+                </option>
+              ))}
           </select>
-          <input type="text" className={styles.header__inpText} placeholder="item....." />
+          <input 
+            type="text" 
+            onChange={(e) => setDescription(e.target.value)}
+            className={styles.header__inpText} 
+            value={description} placeholder="item....." />
           <button className={styles.header__button}>add</button>
         </form>
 
