1c3f56a1ab9e3b1d556040b251dbd1cb03baeac9
Artem Erusov <erusovav@gmail.com>
2024-07-21 19:15:57 +0300

    I made a simple lesson to practice useState

--- src/pages/ultimateReact/items/containerOne/turnCards/turnCards.jsx
+++ src/pages/ultimateReact/items/containerOne/turnCards/turnCards.jsx
@@ -1,5 +1,5 @@
 import styles from './styles.module.scss';
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 import CardBottom from '../../../../../components/UI/cardBottom/cardBottom';
 
 const TurnCards = () => {
@@ -11,18 +11,58 @@ const TurnCards = () => {
     ready: 0,
     column: 2,
     height: '472px',
-    width: '273px'
+    width: '273px',
   };
-  
+  const card = [
+    {
+      id: 1204,
+      front: 'Card 1',
+      back: 'Hello  =)',
+    },
+    {
+      id: 1272,
+      front: 'Card 2',
+      back: 'Hello  =)',
+    },
+    {
+      id: 6789,
+      front: 'Card 3',
+      back: 'Hello  =)',
+    },
+    {
+      id: 3124,
+      front: 'Card 4',
+      back: 'Hello  =)',
+    },
+    {
+      id: 8975,
+      front: 'Card 5',
+      back: 'Hello  =)',
+    },
+    {
+      id: 3321,
+      front: 'Card 6',
+      back: 'Hello  =)',
+    },
+  ];
+  const [selectedId, setSelectedId] = useState(null);
+
   return (
     <Fragment>
-      <div style={{height:componentState.height, width:componentState.width, border: componentState.important ? '2px solid #FFEBB8' : ''}} className={styles.mainContainer}>
-    {/* контент */}
+      <div style={{ height: componentState.height, width: componentState.width, border: componentState.important ? '2px solid #FFEBB8' : '' }} className={styles.mainContainer}>
+        <div className={styles.cardTurn}>
+          {card.map((item) => (
+            <div 
+              className={selectedId === item.id ? styles.cardTurn_active : styles.cardTurn_front} 
+              onClick={() => setSelectedId(item.id !== selectedId ? item.id : null)} key={item.id}>
+              {selectedId === item.id ? item.back : item.front}
+            </div>
+          ))}
+        </div>
       </div>
-      <CardBottom componentState={componentState}/>
+      <CardBottom componentState={componentState} />
     </Fragment>
   );
 };
 
 export default TurnCards;
- 
