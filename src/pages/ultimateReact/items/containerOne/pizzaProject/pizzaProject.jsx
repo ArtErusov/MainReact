00cc73b21cdbfd6dfbf5de4bbe608d79459b05c0
thetancit <thetancit@gmail.com>
2024-07-26 09:22:01 +0000

    I deleted the pizza files from the ReactPizza component in json format

--- src/pages/ultimateReact/items/containerOne/pizzaProject/pizzaProject.jsx
+++ src/pages/ultimateReact/items/containerOne/pizzaProject/pizzaProject.jsx
@@ -3,53 +3,9 @@ import { Fragment } from 'react';
 import CardBottom from '../../../../../components/UI/cardBottom/cardBottom';
 import data from './data.json';
 
-//элемент готов
+import dataPizza from './dataPizza.json';
+
 const PizzaProject = () => {
- 
-  const pizzaData = [
-    {
-      name: 'Focaccia',
-      ingredients: 'Bread with italian olive oil and rosemary',
-      price: 6,
-      photoName: 'img/pizzas/focaccia.jpg',
-      soldOut: false,
-    },
-    {
-      name: 'Pizza Margherita',
-      ingredients: 'Tomato and mozarella',
-      price: 10,
-      photoName: 'img/pizzas/margherita.jpg',
-      soldOut: false,
-    },
-    {
-      name: 'Pizza Spinaci',
-      ingredients: 'Tomato, mozarella, spinach, and ricotta cheese',
-      price: 12,
-      photoName: 'img/pizzas/spinaci.jpg',
-      soldOut: false,
-    },
-    {
-      name: 'Pizza Funghi',
-      ingredients: 'Tomato, mozarella, mushrooms, and onion',
-      price: 12,
-      photoName: 'img/pizzas/funghi.jpg',
-      soldOut: false,
-    },
-    {
-      name: 'Pizza Salamino',
-      ingredients: 'Tomato, mozarella, and pepperoni',
-      price: 15,
-      photoName: 'img/pizzas/salamino.jpg',
-      soldOut: true,
-    },
-    {
-      name: 'Pizza Prosciutto',
-      ingredients: 'Tomato, mozarella, ham, aragula, and burrata cheese',
-      price: 18,
-      photoName: 'img/pizzas/prosciutto.jpg',
-      soldOut: false,
-    },
-  ];
 
   const nowTime = new Date().getHours();
   const checkOpenTime = nowTime >= 12 && nowTime <= 22;
@@ -62,7 +18,7 @@ const PizzaProject = () => {
         <p className={styles.mainContainer__text}>Aauthentic Italian cuisine. 6 creative dishes to choose from. All from our stone oven, all organic, all delicious.</p>
 
         <ul className={styles.card}>
-          {pizzaData.map((item) => (
+          {dataPizza.map((item) => (
             <li className={styles.card__item} key={item.name}>
               <img className={!item.soldOut ? styles.card__item_img : styles.card__item_img_mod} src={item.photoName} alt="" />
               <div className={styles.card__item_flex}>
