210f85c8d73d8466d475fee957ec4face8b7379d
erusovav <erusovav@gmail.com>
2024-07-30 16:08:27 +0000

    Added a close button to the cinema component

--- src/pages/ultimateReact/items/containerTwo/cinema/cinema.jsx
+++ src/pages/ultimateReact/items/containerTwo/cinema/cinema.jsx
@@ -5,22 +5,29 @@ import movieData from './movieData.json';
 import InputText from '../../../../../components/UI/inputText/inputText';
 import MovieItem from './MovieItem';
 import VerticalDivider from '../../../../../components/UI/verticalDivider/verticalDivider';
+import { useState } from 'react';
 
 const Cinema = () => {
   //====================styles====================
-  const borderStyles = { height: data.height, width: data.width, border: data.important === 'important' ? '2px solid #FFEBB8' : data.important === 'ui' ? '2px solid #D8FFD4' : data.important === 'pet' ? '2px solid #ACFFFA' : ''};
- 
+  const borderStyles = { height: data.height, width: data.width, border: data.important === 'important' ? '2px solid #FFEBB8' : data.important === 'ui' ? '2px solid #D8FFD4' : data.important === 'pet' ? '2px solid #ACFFFA' : '' };
   //=============================================
+
+  const [isOpen, setIsOpen] = useState('true')
   return (
     <>
-      <div style={ {borderStyles} } className={styles.mainContainer}>
+      <div style={{ borderStyles }} className={styles.mainContainer}>
         <div className={styles.left}>
-        <InputText placeholder={'Movie title...'} width={'409px'} />
-        {movieData.map((item) => (
-          <MovieItem key={item.imdbID} item={item} />
-        ))}
+
+          <InputText placeholder={'Movie title...'} width={'409px'} />
+          {isOpen ?
+            <div>
+              {movieData.map((item) => (
+                <MovieItem key={item.imdbID} item={item} />
+              ))}
+            </div> : ''}
+          <div onClick={() => setIsOpen(!isOpen)} className={styles.left_close}>{isOpen ? "-" : "+"}</div>
         </div>
-        <VerticalDivider data={data}/>
+        <VerticalDivider data={data} />
       </div>
       <CardBottom componentState={data} />
     </>
