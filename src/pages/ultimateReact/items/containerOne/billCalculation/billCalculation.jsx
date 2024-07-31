3e6f4c0e18a03edd67fe55e28bf53e4d9a910b19
erusovav <erusovav@gmail.com>
2024-07-31 15:03:54 +0000

    Modified the BillCalculator component

--- src/pages/ultimateReact/items/containerOne/billCalculation/billCalculation.jsx
+++ src/pages/ultimateReact/items/containerOne/billCalculation/billCalculation.jsx
@@ -8,15 +8,15 @@ const BillCalculation = () => {
   const borderStyles = data.important === 'important' ? '2px solid #FFEBB8' : data.important === 'ui' ? '2px solid #D8FFD4' : data.important === 'pet' ? '2px solid #ACFFFA' : '';
   // =============================================
 
-  const [test, setTest] = useState()
+  const [score, setScore] = useState()
   const [myBill, setMyBill] = useState("0")
   const [FriendBill, setFriendBill] = useState("0")
 
   const allBill = Number(FriendBill) + Number(myBill)
-  const computation = Math.round(Number(test) + (Number(test) * Number(allBill) / 100))
+  const computation = Math.round(Number(score) + (Number(score) * Number(allBill) / 100))
 
   const reset = () => {
-    setTest(0);
+    setScore("");
     setMyBill(0);
     setFriendBill(0)
   }
@@ -25,7 +25,7 @@ const BillCalculation = () => {
     <>
       <div style={{ height: data.height, width: data.width, border: borderStyles }} className={styles.mainContainer + " " + styles.calc}>
         <p>How much was the bill?</p>
-        <input onChange={(e) => setTest(e.target.value)} type="number" className={styles.calc_input} value={test}/>
+        <input onChange={(e) => setScore(e.target.value)} type="number" className={styles.calc_input} value={score}/>
         <p>How did you like the service?</p>
         <select className={styles.calc_input} value={myBill} onChange={(e) => setMyBill(e.target.value) }>
           <option value="0">Dissatisfied (0%)</option>
@@ -40,9 +40,10 @@ const BillCalculation = () => {
           <option value="10">It was good (10%)</option>
           <option value="20">Amazing! (20%)</option>
         </select>
+
         <div className={styles.botton}>
-          <p className={styles.botton_text}>You pay {computation}</p>
-          <p className={styles.botton_reset}>${test} + ${computation - test} tip</p>
+          <p className={styles.botton_text}>{score ? `You pay ${computation} $` : "Let's start"  }</p>
+          <p className={styles.botton_reset}>{score ?`${score} $ + ${computation - score} $ tip` : "the calculation"}</p>
           <button onClick={() => reset()}>reset</button>   
         </div>
       </div>
