7c8233edc335e2212f78c909a0fc3c30b65d2cf4
Artem Erusov <erusovav@gmail.com>
2024-09-11 22:09:37 +0300

    improved the header component part 2

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import { Fragment, useState } from 'react';
+import { useState } from 'react';
 import styles from './styles.module.scss';
 import iIcon from '/img/svg/i.svg';
 
@@ -7,54 +7,52 @@ const Header = () => {
   const [activeIndex, setActiveIndex] = useState();
   const [activeHelp, setActiveHelp] = useState(false);
   return (
-    <Fragment>
+    <>
       <div className={styles.header}>
-        <div className={styles.flex}>
-          <ul className={styles.list}>
-            {/* <Link to="http://localhost:5173/yandex">
+        <ul className={styles.list}>
+          {/* <Link to="http://localhost:5173/yandex">
               <li onClick={() => setActiveIndex(0)} className={activeIndex == 0 ? styles.list__item + ' ' + styles.list__item_active : styles.list__item}>
                 Yandex
               </li>
             </Link> */}
-            <Link to="http://localhost:5173/ur">
-              <li onClick={() => setActiveIndex(1)} className={activeIndex == 1 ? styles.list__item + ' ' + styles.list__item_active : styles.list__item}>
-                Working with react
-              </li>
-            </Link>
-            {/* <Link to="http://localhost:5173/game">
+          <Link to="http://localhost:5173/ur">
+            <li onClick={() => setActiveIndex(1)} className={activeIndex == 1 ? styles.list__item + ' ' + styles.list__item_active : styles.list__item}>
+              Working with react
+            </li>
+          </Link>
+          {/* <Link to="http://localhost:5173/game">
               <li onClick={() => setActiveIndex(2)} className={activeIndex == 2 ? styles.list__item + ' ' + styles.list__item_active : styles.list__item}>
                 Игры
               </li>
             </Link> */}
-            <Link to="http://localhost:5173/js">
-              <li onClick={() => setActiveIndex(4)} className={activeIndex == 4 ? styles.list__item + ' ' + styles.list__item_active : styles.list__item}>
-                Converted from JS
-              </li>
-            </Link>
-          </ul>
-          <div className={styles.list}>
-            <Link to="http://localhost:5173/pet">
-              <div onClick={() => setActiveIndex(71)} className={activeIndex == 73 ? styles.list__item + ' ' + styles.list__item_active : styles.list__item}>
-                Pet
-              </div>
-            </Link>
-            <Link to="http://localhost:5173/ui">
-              <div onClick={() => setActiveIndex(72)} className={activeIndex == 72 ? styles.list__item + ' ' + styles.list__item_active : styles.list__item}>
-                UI
-              </div>
-            </Link>
-            <Link to="http://localhost:5173/pet">
-              <div onClick={() => setActiveIndex(73)} className={activeIndex == 73 ? styles.list__item + ' ' + styles.list__item_active : styles.list__item}>
-                pet
-              </div>
-            </Link>
-            <div className={styles.help} onClick={() => setActiveHelp(!activeHelp)}>
-              <img src={iIcon} alt="" />
+          <Link to="http://localhost:5173/js">
+            <li onClick={() => setActiveIndex(4)} className={activeIndex == 4 ? styles.list__item + ' ' + styles.list__item_active : styles.list__item}>
+              Converted from JS
+            </li>
+          </Link>
+        </ul>
+        <div className={styles.list}>
+          <Link to="http://localhost:5173/pet">
+            <div onClick={() => setActiveIndex(71)} className={activeIndex == 71 ? styles.list__item + ' ' + styles.list__item_active : styles.list__item}>
+              Technical
             </div>
+          </Link>
+          <Link to="http://localhost:5173/ui">
+            <div onClick={() => setActiveIndex(72)} className={activeIndex == 72 ? styles.list__item + ' ' + styles.list__item_active : styles.list__item}>
+              UI
+            </div>
+          </Link>
+          <Link to="http://localhost:5173/pet">
+            <div onClick={() => setActiveIndex(73)} className={activeIndex == 73 ? styles.list__item + ' ' + styles.list__item_active : styles.list__item}>
+              Pet
+            </div>
+          </Link>
+          <div className={styles.help} onClick={() => setActiveHelp(!activeHelp)}>
+            <img src={iIcon} alt="" />
           </div>
         </div>
       </div>
-      {activeHelp ? (
+      {activeHelp && (
         <div className={styles.bottom}>
           <div className={styles.bottom__pet}></div>
           <p>компоненты котолрые делал безуроков</p>
@@ -65,8 +63,8 @@ const Header = () => {
           <div className={styles.bottom__interesting}></div>
           <p>компоненты для анализа</p>
         </div>
-      ) : null}
-    </Fragment>
+      )}
+    </>
   );
 };
 
