c71802dd892c8327663d5fbaf12db55649e962ce
Artem Erusov <erusovav@gmail.com>
2024-09-13 15:11:34 +0300

    Made changes to the ultimateReact component according to the new design

--- src/pages/ultimateReact/ultimateReact.jsx
+++ src/pages/ultimateReact/ultimateReact.jsx
@@ -1,20 +1,20 @@
 import styles from './styles.module.scss';
 import Divider from '../../components/UI/divider/divider';
 
-{/* ===========================Контейнер 1========================== */} 
+{/* ===========================Сontainer 1========================== */} 
 import ListOfThings from './items/containerOne/listOfThings/listOfThings';
 import HomeWorkCounter from './items/containerOne/homeWorkCounter/homeWorkCounter';
 import WorkingWithStates from './items/containerOne/workingWithStates/workingWithStates';
 import SeparateAccount from './items/containerOne/separateAccount/separateAccount';
 import BillCalculation from './items/containerOne/billCalculation/billCalculation';
-{/* ===========================Контейнер 2========================== */} 
+{/* ===========================Сontainer 2========================== */} 
 import Cinema from './items/containerTwo/cinema/cinema';
 
 const UltimateReact = () => {
   return (
     <>
     {/* ===========================Контейнер 2========================== */}      
-    <Divider title="Контейнер 2"/>     
+    <Divider title="Сontainer 2"/>     
       <div className={styles.grid_two}>   
 {/* ----------------------------Строка 1---------------------------- */}
         <div className={styles.grid_two_one}>
@@ -23,25 +23,26 @@ const UltimateReact = () => {
 
       </div>
 {/* ===========================Контейнер 1========================== */}      
-      <Divider title="Контейнер 1"/>     
+      <Divider title="Сontainer 1"/>     
       <div className={styles.grid_one}>   
+{/* ----------------------------Строка 1---------------------------- */} 
+       <div className={styles.grid_one_five}>
+          <ListOfThings />
+        </div>
+         <div className={styles.grid_one_two }>
+          <SeparateAccount />
+        </div>
+
 {/* ----------------------------Строка 2---------------------------- */} 
-        <div className={styles.grid_one_ten }>
+        
+ <div className={styles.grid_one_three}>
           <BillCalculation />
         </div>
-         <div className={styles.grid_one_eight }>
-          <SeparateAccount />
+        <div className={styles.grid_one_four}>
+          <HomeWorkCounter />
         </div>
         <div className={styles.grid_one_seven}>
           <WorkingWithStates />
-        </div>
-{/* ----------------------------Строка 1---------------------------- */} 
-        <div className={styles.grid_one_five}>
-          <ListOfThings />
-        </div>
-
-        <div className={styles.grid_one_three}>
-          <HomeWorkCounter />
         </div>
       </div>
     </>
